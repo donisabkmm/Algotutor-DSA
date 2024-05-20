2c19289039732c64881c627dc887089751df72a7
DakNetInfo <donisabkmm@gmail.com>
2024-05-20 16:44:10 +0530

    first commit

---
+++ Array/2sum.py
@@ -0,0 +1,37 @@
+"""
+Given an array of integer nums and an integer target, return
+indices of the two numbers such that they add up to the target.
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+You can return the answer in any order.
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+"""
+
+
+testcase = [{
+    "nums" : [2,7,11,15],
+    "target": 9
+},{
+    "nums" : [3,6,8,9,2],
+    "target": 11
+},{
+    "nums" : [2,3,5,9,7],
+    "target": 8
+},{
+    "nums" : [1,8,9,13,15],
+    "target": 28
+},{
+    "nums" : [2,8,3,6,5],
+    "target": 9
+}]
+
+def twosum(nums,target):
+    output = [(i,i+1 )for i in range(len(nums)-1) if nums[i]+nums[i+1] == target ]
+    return output
+
+for i in range(len(testcase)):
+    data = testcase[i]
+    result = twosum(data["nums"],data["target"])
+    print(result)
